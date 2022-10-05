f4c5800bbd0db2b8b9182fa96dfa8bc4a619c9d5
Quentin Topper <quentopp@gmail.com>
2022-10-05 11:57:28 -0700

    Add sfw filter

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,37 @@
 <script lang="ts">
   import Controls from "./lib/Controls.svelte";
+  import CopyrightClause from "./lib/CopyrightClause.html?raw";
   import Terms from "./lib/Terms.html?raw";
   import MyImmortal from "./lib/MyImmortal.html?raw";
   import DontPretend from "./lib/DontPretend.html?raw";
   import LittleBitch from "./lib/LittleBitch.html?raw";
   import InfiniteScroll from "svelte-infinite-scroll";
 
+  let nsfwEnabled = true;
+
+  interface Item {
+    text: string;
+    nsfw: boolean;
+  }
+
   let pageNum = 0;
   let pages = [];
-  const sources = [Terms, DontPretend, MyImmortal, LittleBitch];
+  const sources: Item[] = [
+    { text: Terms, nsfw: false },
+    { text: DontPretend, nsfw: true },
+    { text: MyImmortal, nsfw: true },
+    { text: LittleBitch, nsfw: true },
+  ];
 
   let getNextPage = (count: number) => {
-    let source = count % sources.length;
-    return sources[source];
+    if (nsfwEnabled) {
+      let source = count % sources.length;
+      return sources[source];
+    } else {
+      const sfwSources = sources.filter((p) => !p.nsfw);
+      let source = count % sfwSources.length;
+      return sfwSources[source];
+    }
   };
 
   $: pages = [...pages, getNextPage(pageNum)];
@@ -21,13 +40,21 @@
 <main>
   <div class="terms">
     <h1>Terms and Conditions</h1>
+    <small>
+      <input
+        type="checkbox"
+        checked={nsfwEnabled}
+        on:change={() => (nsfwEnabled = !nsfwEnabled)}
+      />
+      {nsfwEnabled ? "NSFW" : "SFW"}
+    </small>
     <p>Last updated: September 14, 2022</p>
     <p>
       Please read these terms and conditions carefully before using Our Service.
     </p>
     {#each pages as page}
       <br />
-      {@html page}
+      {@html page.text}
     {/each}
     <InfiniteScroll threshold={100} on:loadMore={() => pageNum++} />
   </div>
