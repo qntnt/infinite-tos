988e8a3becd632c91f2e405f7cff926a5f2e016b
Quentin Topper <quentopp@gmail.com>
2022-11-12 13:54:47 -0800

    Fix footer

--- src/App.svelte
+++ src/App.svelte
@@ -98,12 +98,11 @@
     flex-direction: column;
   }
   .controls-container {
-    flex-shrink: 1;
     border-top: 1px solid var(--line-color);
     padding: 1em;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    display: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
   }
 </style>
